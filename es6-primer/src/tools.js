8da6efa9d13fef1fc4b417e416313e0f2e9df168
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-06-08 16:17:18 +0100

    ES6 Primer

--- es6-primer/src/tools.js
+++ es6-primer/src/tools.js
@@ -1,9 +1,11 @@
-var logger = output => {
+const logger = output => {
     console.log(output)
     }
     
-export var appName = "ES6 Review";
-export var dummyFunction = () => { 
-    return "I am a dummy function"; 
+export const appName = "ES6 Review";
+export const dummyFunction = () => { 
+    let feedback = "I am a dummy function";
+    feedback = "I am still a function";
+    return feedback;
 }
 export default logger;
