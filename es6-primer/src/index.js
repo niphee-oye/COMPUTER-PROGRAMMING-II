a82194a16dc09cdabb94502b6cf00ffa4890e4f8
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-06-08 01:42:38 +0100

    ES6 Primer

---
+++ es6-primer/src/index.js
@@ -0,0 +1,2 @@
+import logger, {appName, dummyFunction} from './tools.js' 
+logger(`Welcome, we are having fun with modularity. The name of the app is "${appName}". Also, there is a function that returns: "${dummyFunction()}"`);
